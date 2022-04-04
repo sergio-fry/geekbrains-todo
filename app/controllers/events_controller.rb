c6e3cf1e99213b9c47f56784de78a9839258f398
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-04-04 21:12:22 +0300

    search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def search
+    @query = params[:query]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'events/index'
+  get 'events', to: 'events#search', query: 'Moscow'
+  get 'events/:query', to: 'events#search'
 
   get 'about/index'
 
   get 'not_found', to: redirect('/', status: 302)
   get 'rack', to: ->(_) { [200, {}, ["hello rack"]] }
+  post 'rack', to: ->(_) { [200, {}, ["hello rack with POST"]] }
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
