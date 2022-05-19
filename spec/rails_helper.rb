d20c9085eede3c417e35ab59b4990fdc1946167f
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-05-19 16:14:31 +0300

    stats feature

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,8 +63,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::IntegrationHelpers, type: :integration
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
 
 Shoulda::Matchers.configure do |config|
