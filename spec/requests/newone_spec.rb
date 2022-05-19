21ef01abd7a0188ba2e3e846e47860b537ca0c9b
Sergei O. Udalov <sergei.udalov@gmail.com>
2022-05-19 16:19:59 +0300

    rm view spec

--- spec/requests/newone_spec.rb
+++ spec/requests/newone_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Newones", type: :request do
   describe "GET /index" do
+    before { sign_in create(:user) }
     it "returns http success" do
       get "/newone/index"
       expect(response).to have_http_status(:success)
